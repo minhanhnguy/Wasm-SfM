f46572420f238c8dd774568be7f1dc868583d96e
minhanhnguy <minhanguyen2006@gmail.com>
2025-03-03 11:41:52 -0800

    C++ Image Processing Prototype For Webassembly

--- dist/compile_web.bat
+++ dist/compile_web.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Compiling WebAssembly...
-emcc -O3 math_lib.cpp -s WASM=1 -s EXPORTED_FUNCTIONS="['_add', '_subtract', '_multiply', '_divide']" -s EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap']" -o math_lib.js
+emcc -O3 image_input.cpp -s WASM=1 -s EXPORTED_FUNCTIONS='["_convert_to_gray", "_malloc_image", "_free_image"]' -s EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap']" -o image_input.js
 echo Compilation complete! 
